fc11be8ed8167acb00e7e2d80529deb770c7ea3f
Kim Komatsu <KJKOMATSU@uncg.edu>
2025-03-13 12:07:13 -0400

    Cleaning up libraries needed.

--- code/01_library.R
+++ code/01_library.R
@@ -1,13 +1,15 @@
 # source code for packages to install and load
-install.packages("pacman")
+
+# install.packages("pacman")
 pacman::p_load(here,
                nlme,
                lsmeans,
                performance,
+               PerformanceAnalytics,
                ggpubr,
                codyn, # data generation
                psych, # data generation
-               TaxonStand, # data generation
+               # TaxonStand, # data generation
                WorldFlora, # data generation
                MASS, # Q1 analysis
                foreign, # Q1 analysis
