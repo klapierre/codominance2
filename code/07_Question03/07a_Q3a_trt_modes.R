a4faa2c77014e6d325f24425ebdc2202e31a5fbb
Kim Komatsu <KJKOMATSU@uncg.edu>
2025-07-02 16:53:16 -0400

    Re-adding code to remove Eldridge data #2.

--- code/07_Question03/07a_Q3a_trt_modes.R
+++ code/07_Question03/07a_Q3a_trt_modes.R
@@ -160,7 +160,7 @@ modeAll <- modeTrt %>%
 summaryTableAll <- xtabs(~ trt_ctl + lump_mode_cat, data = modeAll)
 
 print(chisq <- chisq.test(summaryTableAll))
-# X-squared = 10.173, df = 2, p-value = 0.00618
+# X-squared = 9.3228, df = 2, p-value = 0.009453
 
 mosaicplot(summaryTableAll, shade = TRUE, las=2,
            main = "summaryTableAll")
@@ -199,7 +199,7 @@ anova(m1,m2,m3,m4,m5,m6)
 modeSiteCodom <- xtabs(~ lump_mode_site_cat + lump_mode_trt_cat, data = modeTrt)
 
 print(chisq <- chisq.test(modeSiteCodom))
-# X-squared = 197.14, df = 4, p-value < 2.2e-16
+# X-squared = 190.19, df = 4, p-value < 2.2e-16
 
 mosaicplot(modeSiteCodom, shade = TRUE, las=2,
            main = "modeSiteCodom")
@@ -224,7 +224,7 @@ ggplot(summaryModeSiteCodom, aes(x=lump_mode_site_cat , y=lump_mode_trt_cat)) +
 modeTrtCodom <- xtabs(~ lump_mode_trt_cat + trt_type, data = modeTrt)
 
 print(chisq <- chisq.test(modeTrtCodom))
-# X-squared = 57.796, df = 24, p-value = 0.0001299
+# X-squared = 57.59, df = 24, p-value = 0.0001387
 
 mosaicplot(modeTrtCodom, shade = TRUE, las=2,
            main = "modeTrtCodom")
