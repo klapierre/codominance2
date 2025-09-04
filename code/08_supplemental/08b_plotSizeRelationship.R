134f10f5b311d6bae437fc1e2233e9cf5c9e84fc
Kim Komatsu <KJKOMATSU@uncg.edu>
2025-09-04 16:31:37 -0400

    Plot size relationship with codominance.

--- code/08_supplemental/08b_plotSizeRelationship.R
+++ code/08_supplemental/08b_plotSizeRelationship.R
@@ -17,152 +17,53 @@ theme_update(axis.title.x=element_text(size=40, vjust=-0.35, margin=margin(t=15)
 
 
 
-### calculate richness at plot level
+### get plot size data for each database
 
-plotSize <- readRDS("data/modeSite.rds") %>% 
-  separate(exp_unit, into=c('site_code', 'project_name', 'community_type', 
-                            'plot_id', 'treatment', 'calendar_year'), 
-           sep='::', remove=F) %>% 
-  left_join(readRDS("data/expInfo.rds"))
+corre <- read.csv('C:\\Users\\kjkomatsu\\OneDrive - UNCG\\manuscripts\\1_first author\\codominance\\data\\CoRRE\\corre_plot_size.csv') %>% 
+  rename(plot_size=plot_size_m2) %>% 
+  select(site_code, project_name, community_type, plot_size)
 
+gex <- read.csv('C:\\Users\\kjkomatsu\\OneDrive - UNCG\\manuscripts\\1_first author\\codominance\\data\\GEx\\GEx-metadata-with-other-env-layers-v2.csv') %>% 
+  rename(plot_size=PlotSize,
+         site_code=site) %>% 
+  mutate(project_name=0, community_type=0) %>% 
+  select(site_code, project_name, community_type, plot_size)
 
-
-### calculate mode of modes for richness
-
-# Calculate mode across years for all plots ----------------------------------------------------------
-
-# create a dataframe of codominant group for plots with a single timepoint (because can't calculate mode of singleton)
-singletonCodomPlotYear <- plotRichness %>% 
-  group_by(database, site_code, project_name, community_type, plot_id, trt_type, treatment) %>% 
-  mutate(length=length(plot_id)) %>% 
-  ungroup() %>% 
-  filter(length==1) %>% 
-  rename(plot_rich=richness) %>% 
-  dplyr::select(database, site_code, project_name, community_type, plot_id, trt_type, treatment, plot_rich) 
-
-# calculate mode across years for all plots
-modePlotTrue <- plotRichness %>%  
-  group_by(database, site_code, project_name, community_type, plot_id, trt_type, treatment) %>% 
-  reframe(plot_rich = DescTools::Mode(richness)) %>% # mode function must be capital here 
-  ungroup() %>% 
-  filter(!is.na(plot_rich)) %>%
-  group_by(database, site_code, project_name, community_type, plot_id, trt_type, treatment) %>% 
-  summarise(plot_rich=round(mean(plot_rich), digits=0), .groups='drop') %>% # calculate mean for ties
-  rbind(singletonCodomPlotYear)
-
-# for plots with singleton ties for modes, calculate mean and round to nearest integer
-multipleMode <- plotRichness %>% 
-  select(database, site_code, project_name, community_type, plot_id, trt_type, treatment, richness, calendar_year) %>% 
-  unique() %>% 
-  full_join(modePlotTrue) %>% 
-  filter(is.na(plot_rich)) %>%
-  group_by(database, site_code, project_name, community_type, plot_id, trt_type, treatment) %>% 
-  summarise(plot_rich=round(mean(richness), digits=0), .groups='drop')
-
-# bind dataframes for averaged ties and true modes at plot level
-modePlot <- rbind(modePlotTrue, multipleMode) %>% 
-  left_join(readRDS("data/modePlot.rds")) %>% 
-  mutate(group=ifelse(plot_codom %in% c(2,3), 'codominated', 
-               ifelse(plot_codom==1, 'monodominated', 'even')))
-
-
-# Calculate mode across all control plots for each experiment ------------------
-
-# create a dataframe of codominant group for experiments with a single plot (because can't calculate mode of singleton)
-singletonCodomPlot <- modePlot %>% 
-  select(-plot_codom) %>% 
-  filter(trt_type=='control') %>% 
-  group_by(database, site_code, project_name, community_type) %>% 
-  mutate(length=length(community_type)) %>% 
-  ungroup() %>% 
-  filter(length==1) %>% 
-  rename(site_rich=plot_rich) %>% 
-  dplyr::select(database, site_code, project_name, community_type, site_rich) 
-
-# calculate mode across plots for each experiment, dropping those with ties
-modeSiteTrue <- modePlot %>%
-  select(-plot_codom) %>% 
-  filter(trt_type=='control') %>%
-  group_by(database, site_code, project_name, community_type) %>% # mode generated from these
-  reframe(site_rich = DescTools::Mode(plot_rich)) %>%  
-  ungroup() %>% 
-  group_by(database, site_code, project_name, community_type) %>% 
-  summarise(site_rich=round(mean(site_rich), digits=0), .groups='drop') %>% # calculate mean for ties
-  filter(!is.na(site_rich)) %>% 
-  rbind(singletonCodomPlot)
-
-# for plots with ties for modes, calculate mean and round to nearest integer
-multipleModeProj <- modePlot %>% 
-  select(-plot_codom) %>% 
-  filter(trt_type=='control') %>% 
-  select(database, site_code, project_name, community_type, plot_id, plot_rich) %>% 
-  full_join(modeSiteTrue) %>% 
-  filter(is.na(site_rich)) %>%
-  group_by(database, site_code, project_name, community_type) %>% 
-  summarise(site_rich=round(mean(plot_rich), digits=0), .groups='drop')
-
-# bind dataframes for average ties and true modes at site level
-modeSite <- rbind(modeSiteTrue, multipleModeProj) %>% 
-  left_join(readRDS("data/envData.rds")) %>% 
-  left_join(readRDS("data/modeSite.rds")) %>% 
-  mutate(group=ifelse(mode_site %in% c(2,3), 'codominated', 
-                      ifelse(mode_site==1, 'monodominated', 'even')))
+plotSize <- rbind(corre, gex) %>% 
+  full_join(readRDS("data/modeSite.rds")) %>% 
+  mutate(plot_size=ifelse(database=='nutnet', 1, plot_size),
+         group=ifelse(lumpMode==1, 'monodominated',
+               ifelse(lumpMode==2, 'codominated',
+               'even'))) %>% 
+  filter(!is.na(plot_size))
 
 
 
 # ordinal logistic regressions ------------------
 
-allModel <- polr(group ~ richness, data = plotRichness, Hess=T) 
-nullAllModel <- polr(group ~ 1, data = plotRichness, Hess=T)
-summary(allModel)
-lrt <- anova(allModel, nullAllModel)
-
-plotModel <- polr(group ~ plot_rich, data = modePlot, Hess=T) 
-nullPlotModel <- polr(group ~ 1, data = modePlot, Hess=T)
-summary(plotModel)
-lrt <- anova(plotModel, nullPlotModel)
-
-siteModel <- polr(group ~ site_rich, data = modeSite, Hess=T) 
-nullSiteModel <- polr(group ~ 1, data = modeSite, Hess=T)
+siteModel <- polr(as.factor(group) ~ plot_size, data = plotSize, Hess=T) 
+nullSiteModel <- polr(as.factor(group) ~ 1, data = plotSize, Hess=T)
 summary(siteModel)
 lrt <- anova(siteModel, nullSiteModel)
 
 
-
 # anovas (backwards testing) ------------------
-summary(allBackwardModel <- aov(richness ~ group, data=plotRichness))
-TukeyHSD(allBackwardModel)
 
-summary(plotBackwardModel <- aov(plot_rich ~ group, data=modePlot))
-TukeyHSD(plotBackwardModel)
-
-summary(siteBackwardModel <- aov(site_rich ~ group, data=modeSite))
+summary(siteBackwardModel <- aov(plot_size ~ as.factor(group), data = plotSize))
 TukeyHSD(siteBackwardModel)
 
 
 # figures ------------------
 
-plotRichness$group <- factor(plotRichness$group, levels=c('even', 'codominated', 'monodominated'))
-modePlot$group <- factor(modePlot$group, levels=c('even', 'codominated', 'monodominated'))
-modeSite$group <- factor(modeSite$group, levels=c('even', 'codominated', 'monodominated'))
+plotSize$group <- factor(plotSize$group, levels=c('even', 'codominated', 'monodominated'))
 
-allFig <- ggplot(data=plotRichness, aes(x=group, y=richness)) +
-  geom_boxplot() +
-  xlab('') + ylab('Richness (all plots and years)') +
+ggplot(data=barGraphStats(data=plotSize, variable="plot_size", byFactorNames=c("group")), aes(x=group, y=mean)) +
+  geom_point(size=9) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0, size=6) +
+  geom_errorbar(aes(ymin=mean-1.96*se, ymax=mean+1.96*se), width=0.2, size=3) +
+  geom_point(data=plotSize, aes(x=group, y=plot_size), size=4, color='grey') +
+  xlab('') + ylab('Plot Size') +
   coord_flip()
-
-plotModeFig <- ggplot(data=modePlot, aes(x=group, y=plot_rich)) +
-  geom_boxplot() +
-  xlab('') + ylab('Richness (plot mode)') +
-  coord_flip()
-
-siteModeFig <- ggplot(data=modeSite, aes(x=group, y=site_rich)) +
-  geom_boxplot() +
-  xlab('') + ylab('Richness (site mode)') +
-  coord_flip()
-
-
-grid.arrange(allFig, plotModeFig, siteModeFig, ncol=3, nrow=1)
-#export at 4000x500
+#export at 1500x500
 
 
