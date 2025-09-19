fffb6bc98ade947a469b5d3fbecd2cf021833f67
Kim Komatsu <KJKOMATSU@uncg.edu>
2025-09-19 15:49:49 -0400

    If we wanted to categorize treatments into multiple stress vs multiple resource, but it doesn't really bin nicely because of lots of "other" mixed in.

--- code/03_generatingCodominanceData/03d_combine_data.R
+++ code/03_generatingCodominanceData/03d_combine_data.R
@@ -168,6 +168,25 @@ individualExperiments <- rbind(corre, gex, nutnet) %>%
   mutate(trt_type2=ifelse(trt_type3 %in% c('N','P','K','N*P','mult_nutrient','herb_removal','disturbance','CO2',
                                            'irr','drought','temp','multiple_trts', 'control'), trt_type3, 'other')) %>%
   filter(!grepl("plant_mani", trt_type)) %>%  #filter out any treatment that directly manipulates plant species
+  # mutate(trt_category=ifelse(trt_type %in% c('N','P','K','CO2','irr'), 'resource',
+  #                     ifelse(trt_type %in% c('drought','herb_removal','temp', 'stress',
+  #                     ifelse(trt_type %in% c("drought*temp"), 'multiple_stress',
+  #                     ifelse(trt_type %in% c("irr*CO2","N*irr*CO2","mult_nutrient*irr","N*CO2","N*irr",), 'multiple_resource',
+  #                     ifelse(trt_type %in% c("CO2*temp","drought*CO2*temp","drought*mow_clip","drought*temp*mow_clip",
+  #                                          "herb_removal*mow_clip","irr*CO2*temp","irr*mow_clip","irr*herb_removal",
+  #                                          "irr*temp*mow_clip","N*CO2*temp","N*irr*mow_clip","N*P*burn*graze",
+  #                                          "N*irr*CO2*temp","N*mow_clip","N*burn","N*burn*graze","N*disturbance",
+  #                                          "P*burn*graze","P*burn*mow_clip","N*drought","N*herb_removal","P*herb_removal",
+  #                                          "N*irr*temp","N*temp","mult_nutrient*temp", "N*P*temp","mult_nutrient*mow_clip",
+  #                                          "N*burn*mow_clip","N*P*burn","N*P*mow_clip","P*burn","P*mow_clip",
+  #                                          "mult_nutrient*herb_removal","mult_nutrient*herb_removal*mow_clip",
+  #                                          "temp*mow_clip","irr*temp","C*stone","irr*plant_mani",
+  #                                          'irr*plant_mani*herb_removal',"mult_nutrient*drought","mult_nutrient*fungicide",
+  #                                          "mult_nutrient*plant_mani","mult_nutrient*plant_mani*herb_removal","N*fungicide",
+  #                                          "N*P*burn*mow_clip","N*P*plant_mani","N*plant_mani","N*plant_mani*disturbance",
+  #                                          "N*plant_mani*mow_clip","N*stone","N*temp*fungicide","P*plant_mani",
+  #                                          "plant_mani*disturbance","plant_mani*herb_removal","plant_mani*mow_clip",
+  #                                          "precip_vari*temp","temp*fungicide"), 'multiple_mixed', 'other'))))))) %>% 
   dplyr::select(-trt_type, -trt_type3) %>% 
   rename(trt_type=trt_type2) %>% 
   select(-plot_size_m2, -plot_number, -plot_permenant) %>% 
